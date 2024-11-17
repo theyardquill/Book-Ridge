e7980ad9b88207f73ab162caee6acf47b201086a
Strucker Writing Services . <struckerwritingservices@gmail.com>
2024-11-18 01:43:55 +0300

    Fix info.tsx to "use client" rendering

--- store/components/info.tsx
+++ store/components/info.tsx
@@ -1,13 +1,23 @@
+"use client";
+
 import { Product } from "@/types";
 import Currency from "@/components/ui/currency";
 import Button from "@/components/ui/button";
 import { ShoppingCart } from "lucide-react";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
     data: Product;
 }
+
 const Info: React.FC<InfoProps> = ({ data }) => {
-    return ( 
+    const cart = useCart();
+
+    const onAddToCart = () => {
+        cart.addItem(data);
+    };
+
+    return (
         <div>
             <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
             <div className="flex items-end justify-between mt-3">
@@ -19,23 +29,24 @@ const Info: React.FC<InfoProps> = ({ data }) => {
             <div className="flex flex-col gap-y-6">
                 <div className="flex items-center gap-x-4">
                     <h3 className="font-semibold text-black">Size:</h3>
-                        <div>
-                            {data?.size?.value}
-                        </div>
+                    <div>{data?.size?.value}</div>
                 </div>
                 <div className="flex items-center gap-x-4">
                     <h3 className="font-semibold text-black">Color:</h3>
-                    <div className="w-6 h-6 border border-gray-600 rounded-full" style={{ backgroundColor: data?.color?.value }}/>
+                    <div
+                        className="w-6 h-6 border border-gray-600 rounded-full"
+                        style={{ backgroundColor: data?.color?.value }}
+                    />
                 </div>
             </div>
             <div className="flex items-center mt-10 gap-x-4">
-                <Button className="flex items-center gap-x-2">
+                <Button onClick={onAddToCart} className="flex items-center gap-x-2">
                     Add To Cart
                     <ShoppingCart />
                 </Button>
             </div>
         </div>
-     );
-}
- 
+    );
+};
+
 export default Info;
