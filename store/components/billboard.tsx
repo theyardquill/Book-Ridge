d69f1723d826d1bdbb67b2bb170dc3b56fe8c1e6
Strucker Writing Services . <struckerwritingservices@gmail.com>
2024-11-18 20:20:28 +0300

    Update billboard.tsx

--- store/components/billboard.tsx
+++ store/components/billboard.tsx
@@ -19,7 +19,7 @@ const Billboard: React.FC<BillboardProps> = ({ data }) => {
 
                 {/* Content */}
                 <div className='relative z-20 flex flex-col items-center justify-center w-full h-full text-center gap-y-8'>
-                    <div className='max-w-xs text-3xl text-white font-bold sm:text-5xl lg:text-6xl sm:max-w-xl'>
+                    <div className='max-w-xs text-3xl text-[#994C00] font-bold sm:text-5xl lg:text-6xl sm:max-w-xl'>
                         {data?.label}
                     </div>
                 </div>
