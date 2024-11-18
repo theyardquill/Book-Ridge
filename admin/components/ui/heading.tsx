9a940b832ea05585eab13ba902c30788251dd9fb
Strucker Writing Services . <struckerwritingservices@gmail.com>
2024-11-18 22:22:15 +0300

    Update heading.tsx

--- admin/components/ui/heading.tsx
+++ admin/components/ui/heading.tsx
@@ -6,8 +6,8 @@ interface HeadingProps {
 export const Heading: React.FC<HeadingProps> = ({  title, description }) => {
     return (
         <div>
-            <h2 className="text-3xl text-[#994C00] font-bold tracking-tight">{title}</h2>
-            <p className="text-sm text-[#994C00] text-muted-foreground">{description}</p>
+            <h2 className="text-3xl  font-bold tracking-tight">{title}</h2>
+            <p className="text-sm  text-muted-foreground">{description}</p>
         </div>
     )
 }
