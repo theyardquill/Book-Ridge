6c167d8eb2d2260308e49017f195c833d89798a6
Strucker Writing Services . <struckerwritingservices@gmail.com>
2024-11-18 22:08:59 +0300

    Update navbar.tsx

--- store/components/navbar.tsx
+++ store/components/navbar.tsx
@@ -15,16 +15,16 @@ const Navbar = async () => {
                 <div className="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
                     {/* Branding */}
                     <Link href="/" className="text-[#994C00] md:order-1 flex items-center">
-                        <p className="text-md font-bold">Cabimah Adventures</p>
+                        <p className="text-xl font-bold">Cabimah Adventures</p>
                     </Link>
                     
                     {/* Navigation Links */}
-                    <div className="text-gray-500 order-3 w-full md:w-auto md:order-2">
+                    <div className="text-gray-500 text-md order-3 w-full md:w-auto md:order-2">
                         <MainNav data={categories || []} />
                     </div>
 
                     {/* Actions */}
-                    <div className="order-2 md:order-3">
+                    <div className="order-2 text-md md:order-3">
                         <NavbarActions />
                     </div>
                 </div>
