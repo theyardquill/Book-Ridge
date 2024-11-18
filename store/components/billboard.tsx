e6e8b2caf08ca53bb92aa87f1798edcaaf10b05e
Strucker Writing Services . <struckerwritingservices@gmail.com>
2024-11-18 04:37:28 +0300

    Styling: Overlay on billboard Background Image

--- store/components/billboard.tsx
+++ store/components/billboard.tsx
@@ -1,20 +1,24 @@
 import { Billboard as BillboardType } from '@/types';
 
 interface BillboardProps {
-    data: BillboardType
-};
+    data: BillboardType;
+}
 
 const Billboard: React.FC<BillboardProps> = ({ data }) => {
     return (
         <div className='p-4 overflow-hidden sm:p-6 lg:p-8 rounded-xl'>
-            <div
-                className='rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden'
-                style={{ backgroundImage: `url(${data?.imageUrl})` }}>
+            <div className='relative rounded-xl aspect-square md:aspect-[2.4/1] overflow-hidden'>
+                {/* Background Image */}
+                <div
+                    className='absolute inset-0 bg-cover bg-center'
+                    style={{ backgroundImage: `url(${data?.imageUrl})` }}
+                />
                 
-                {/* Overlay div with opacity */}
-                <div className='absolute inset-0 bg-black opacity-50' />  {/* Adjust opacity as needed */}
+                {/* Overlay */}
+                <div className='absolute inset-0 bg-black/30 z-10' />
 
-                <div className='flex flex-col items-center justify-center w-full h-full text-center gap-y-8'>
+                {/* Content */}
+                <div className='relative z-20 flex flex-col items-center justify-center w-full h-full text-center gap-y-8'>
                     <div className='max-w-xs text-3xl text-white font-bold sm:text-5xl lg:text-6xl sm:max-w-xl'>
                         {data?.label}
                     </div>
@@ -22,6 +26,6 @@ const Billboard: React.FC<BillboardProps> = ({ data }) => {
             </div>
         </div>
     );
-}
+};
 
 export default Billboard;
