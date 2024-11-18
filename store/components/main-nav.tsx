2a51252e560b526820758f7fc53299b643ca034f
Strucker Writing Services . <struckerwritingservices@gmail.com>
2024-11-19 00:27:40 +0300

    Fix: PAssing Classname Error

--- store/components/main-nav.tsx
+++ store/components/main-nav.tsx
@@ -1,31 +1,30 @@
-"use client"
-import { cn } from '@/lib/utils';
-import { Category } from '@/types';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation'
-
 interface MainNavProps {
-    data: Category[] | []
+    data: Category[] | [];
+    className?: string;  // Allow className to be passed in
 }
 
-const MainNav: React.FC<MainNavProps> = ({ data }) => {
+const MainNav: React.FC<MainNavProps> = ({ data, className }) => {
     const pathname = usePathname();
 
     const routes = data.map(route => ({
         href: `/category/${route.id}`,
         label: route.name,
         active: pathname === `/category/${route.id}`
-    }))
+    }));
 
     return (
-        <nav className='flex items-center mx-6 space-x-4 lg:space-x-6'>
+        <nav className={`flex items-center mx-6 space-x-4 lg:space-x-6 ${className}`}>
             {routes.map(route => (
-                <Link key={route.href} href={route.href} className={cn('text-md font-bold transition-colors text-green-500', route.active ? 'text-black' : 'text-neutral-500')}>
+                <Link
+                    key={route.href}
+                    href={route.href}
+                    className={cn('text-md font-bold transition-colors text-green-500', route.active ? 'text-black' : 'text-neutral-500')}
+                >
                     {route.label}
                 </Link>
             ))}
         </nav>
-    )
-}
+    );
+};
 
 export default MainNav;
