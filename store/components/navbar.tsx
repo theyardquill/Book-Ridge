3142ea11614a5508df36fafbba0a1d2cba12cf35
Strucker Writing Services . <struckerwritingservices@gmail.com>
2024-11-18 20:13:45 +0300

    Update navbar.tsx

--- store/components/navbar.tsx
+++ store/components/navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = async () => {
             <Container>
                 <div className="relative flex items-center h-16 px-4 sm:px-6 lg:px-8">
                     <Link href="/" className="flex ml-4 lg:ml-0 gap-x-2">
-                        <p className="text-xl font-bold">Abimah Adventures</p>
+                        <p className="text-xl font-bold">Cabimah Adventures</p>
                     </Link>
                     <MainNav data={categories || []} />
                     <NavbarActions />
