0bd4f1f66e81c44ea73c6a543b94ed1a33c3b6e3
Strucker Writing Services . <struckerwritingservices@gmail.com>
2024-11-19 03:42:25 +0300

    Update billboard.tsx

--- store/components/billboard.tsx
+++ store/components/billboard.tsx
@@ -1,31 +1,33 @@
-import { Billboard as BillboardType } from '@/types';
+"use client";
+
+import { Billboard as BillboardType } from "@/types";
 
 interface BillboardProps {
-    data: BillboardType;
+  data: BillboardType;
 }
 
 const Billboard: React.FC<BillboardProps> = ({ data }) => {
-    return (
-        <div className='p-4 overflow-hidden sm:p-6 lg:p-8 rounded-xl'>
-            <div className='relative rounded-xl aspect-square md:aspect-[2.4/1] overflow-hidden'>
-                {/* Background Image */}
-                <div
-                    className='absolute inset-0 bg-cover bg-center'
-                    style={{ backgroundImage: url(${data?.imageUrl}) }}
-                />
-                
-                {/* Overlay */}
-                <div className='absolute inset-0 bg-black/50 z-10' />
-
-                {/* Content */}
-                <div className='relative z-13 flex flex-col items-center justify-center w-full h-full text-center gap-y-8'>
-                    <div className='max-w-xs text-3xl text-white font-bold sm:text-5xl lg:text-6xl sm:max-w-xl'>
-                        {data?.label}
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="p-4 overflow-hidden sm:p-6 lg:p-8 rounded-xl">
+      <div className="relative rounded-xl aspect-square md:aspect-[2.4/1] overflow-hidden">
+        {/* Background Image */}
+        <div
+          className="absolute inset-0 bg-cover"
+          style={{ backgroundImage: `url(${data?.imageUrl})` }}
+        />
+        {/* Overlay */}
+        <div className='absolute inset-0 bg-black/50'/>
+        <div className="relative flex flex-col justify-center items-center h-full text-center text-white gap-y-8">
+          <div className="max-w-xs sm:max-w-sm md:max-w-lg lg:max-w-2xl">
+            <h1 className="text-2xl sm:text-4xl md:text-5xl font-bold">
+              {data?.label}
+            </h1>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Billboard;
+
