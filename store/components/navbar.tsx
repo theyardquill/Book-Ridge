b2ec3f96c390c78834176107cc1376f818462cf8
Strucker Writing Services . <struckerwritingservices@gmail.com>
2024-12-14 23:47:47 +0300

    Update navbar.tsx

--- store/components/navbar.tsx
+++ store/components/navbar.tsx
@@ -8,39 +8,39 @@ import Image from "next/image";
 export const revalidate = 0;
 
 const Navbar = async () => {
-  const categories = await getCategories();
+    const categories = await getCategories();
 
-  return (
-    <nav className="bg-white shadow-md border-b border-[#994C00] shadow-[#994C00] w-full px-4">
-      <Container>
-        <div className="flex items-center justify-between flex-wrap md:flex-nowrap md:h-16 h-28 mx-auto">
-          {/* Branding */}
-          <Link href="/" className="flex items-center space-x-2 text-[#994C00]">
-            {/* Logo */}
-            <Image
-              src="/Logo.png" // Path to your logo
-              alt="Cabimah Adventures Logo"
-              width={64} // Base width
-              height={64} // Base height
-              className="w-16 h-16"
-              priority // Ensures the logo loads quickly
-            />
-            <span className="text-lg font-bold md:text-xl">Cabimah Adventures</span>
-          </Link>
+    return (
+        <nav className="bg-white shadow-md border-b border-[#994C00] shadow-[#994C00] w-full px-8 md:px-4">
+            <Container>
+                <div className="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
+                    {/* Branding */}
+                    <Link href="/" className="text-[#994C00] md:order-1 flex items-center space-x-1 -ml-6">
+                        {/* Logo */}
+                        <Image
+                        src="/Logo.png" // Path to your logo
+                        alt="Cabimah Adventures Logo"
+                        width={120} // Base width
+                        height={120} // Base height
+                        className="w-16 h-16 sm:w-12 sm:h-12 md:w-16 md:h-16 lg:w-18 lg:h-18" // Responsive sizes
+                    />
 
-          {/* Navigation Links */}
-          <div className="w-full md:w-auto mt-4 md:mt-0">
-            <MainNav data={categories || []} />
-          </div>
+                        <p className="text-md md:text-lg lg:text-xl -ml-12 -mr-12 font-bold">Cabimah Adventures</p>
+                    </Link>
 
-          {/* Actions */}
-          <div className="mt-4 md:mt-0">
-            <NavbarActions />
-          </div>
-        </div>
-      </Container>
-    </nav>
-  );
+                    {/* Navigation Links */}
+                    <div className="order-3 w-full md:w-auto md:order-2">
+                        <MainNav data={categories || []} />
+                    </div>
+
+                    {/* Actions */}
+                    <div className="order-2  md:order-3">
+                        <NavbarActions />
+                    </div>
+                </div>
+            </Container>
+        </nav>
+    );
 };
 
 export default Navbar;
