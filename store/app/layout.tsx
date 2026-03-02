c18a65df7af03e223db9fda597211ab137e58433
theyardquill <struckerwritingservices@gmail.com>
2026-03-02 12:37:18 +0300

    favicon

--- store/app/layout.tsx
+++ store/app/layout.tsx
@@ -27,6 +27,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/public/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/public/favicon.svg" />
+<link rel="shortcut icon" href="/public/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-title" content="Bookridge" />
+<link rel="manifest" href="/public/site.webmanifest" />
+      </head>
       <body className={urban.className}>
         <ModalProvider />
         <ToastProvider />
